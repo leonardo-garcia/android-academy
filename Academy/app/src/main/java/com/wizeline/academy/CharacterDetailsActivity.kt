66eb5ac1a957a59af11b2491b9c4bf9d9b904d9d
Leonardo Garcia <leo.equih@gmail.com>
2019-09-24 14:51:54 -0500

    Add CharacterDetailsActivity

---
+++ Academy/app/src/main/java/com/wizeline/academy/CharacterDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.wizeline.academy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_details)
+    }
+}
